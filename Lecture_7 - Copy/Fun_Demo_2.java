626d0c36cb56e63f732fc75dd141ba7f77b42bef
Deepti Gupta <deepti20gupta05@gmail.com>
2025-08-06 11:56:05 +0530

    main

---
+++ Lecture_7 - Copy/Fun_Demo_2.java
@@ -0,0 +1,20 @@
+package lec7;
+
+public class Fun_Demo_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		System.out.println("hello");
+		int ans=Add();
+		System.out.println(ans);
+	}
+	
+	public static int Add() {
+		int a=8;
+		int b=7;
+		int c=a+b;
+		return c;
+	}
+	
+
+}
