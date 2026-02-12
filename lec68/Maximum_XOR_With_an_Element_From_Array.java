09f36d0f73f9b381c6eebdb092676aef912dfd25
Deepti Gupta <deepti20gupta05@gmail.com>
2026-02-12 12:43:02 +0530

    lec68 added

--- lec68/Maximum_XOR_With_an_Element_From_Array.java
+++ lec68/Maximum_XOR_With_an_Element_From_Array.java
@@ -1,6 +1,10 @@
-package lec67;
+package lec68;
 
-public class Maximum_XOR_of_Two_Numbers_in_an_Array {
+import java.util.*;
+
+public class Maximum_XOR_With_an_Element_From_Array {
+
+//	https://leetcode.com/problems/maximum-xor-with-an-element-from-array/
 	
 	static class Trie{
 		class Node{
@@ -66,22 +70,46 @@ public class Maximum_XOR_of_Two_Numbers_in_an_Array {
 			return ans;
 		}
 	}
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr= {3,10,5,25,2,8};
+		int[] arr= {0,1,2,3,4};
+		int[][] queries= {{3,1},{1,3},{5,6}};
+		int n=queries.length;
+		int[][] q=new int[n][3];
+	
+		for(int i=0; i<n; i++) {
+			q[i][0]=queries[i][0];
+			q[i][1]=queries[i][1];
+			q[i][2]=i;
+		}
+		
+		Arrays.sort(q,(a,b)->a[1]-b[1]);
+		Arrays.sort(arr);
+		
+		int[] ans=new int[n];
 		Trie t=new Trie();
-		for(int x:arr) {
-			t.add(x);
+		int j=0;
+		for(int[] a:q) {
+			while(j<n && arr[j]<=a[1]) {
+				t.add(arr[j]);
+				j++;
+			}
+			
+			if(j==0) {
+				ans[a[2]]=-1;
+			}
+			else {
+				int v=t.getMaxXor(a[0]);
+				ans[a[2]]=v;
+			}
 		}
-		
-		int ans=0;
-		for(int x:arr) {
-			ans=Math.max(ans, t.getMaxXor(x));
+	
+		for(int i:ans) {
+			System.out.print(i+" ");
 		}
-		System.out.println(ans);
-		
-
 	}
 
 }
+
+
